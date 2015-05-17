93301cc489d5909eaae4640612227132d6d6a323
bluebirrd <anna.kurilo@outlook.com>
2015-05-17 10:25:10 +0300

    Change the type of tail to LinkedList<Integer>, create toRL() method

--- src/RLnumber.java
+++ src/RLnumber.java
@@ -1,20 +1,39 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class RLnumber {
 
+    private static final int K_MAX = 53;
+    private static final int K_MIN = -11;
     private int sign;
     private int length;
-    private int[] tail;
+    private LinkedList<Integer> tail;
 
     public RLnumber() {
         sign = 0;
         length = 0;
-        tail = new int[] {0};
+        tail = new LinkedList<>();
     }
 
     public RLnumber(int sign, int length, int[] tail) {
         this.sign = sign;
         this.length = length;
-        this.tail = tail.clone();
+        for (int i : tail) this.tail.add(i);
+    }
+
+    /**
+     * creates an RLnumber from LinkedList of Integers by making a hard copy of this LinkedList
+     * (using get() to retrieve the elements from incoming LinkedList)
+     * @param sign - sign of a number
+     * @param length - length of RL tail
+     * @param tail - significant 1s in binary presentation of a given number
+     */
+    public RLnumber(int sign, int length, List<Integer> tail) {
+        this.sign = sign;
+        this.length = length;
+        for (Integer i : tail) {
+            this.tail.add(i);
+        }
     }
 
     public int getSign() {
@@ -33,13 +52,38 @@ public class RLnumber {
         this.length = length;
     }
 
-    public int[] getTail() {
+    public LinkedList<Integer> getTail() {
         return tail;
     }
-
-    public void setTail(int[] tail) {
+//    TODO create a setTail method that converts int[] to LinkedList<Integer>
+    public void setTail(LinkedList<Integer> tail) {
         this.tail = tail;
     }
 
+    public void countLength() {
+        length = tail.size();
+    }
 
+    public static RLnumber toRL(double number) {
+        RLnumber result = null;
+        List<Integer> tail = new LinkedList<>();
+        /*
+        double intPart = Math.floor(number);
+        double fraction = number - intPart;
+        */
+        int sign = (number >= 0) ? 0 : 1;
+        number = Math.abs(number);
+        for (int k = K_MAX; k > K_MIN; k--) {
+            double number1 = number;
+            number1 = number1 - Math.scalb(1,k);
+            if (number1 < 0) {
+                number1 = number;
+            } else {
+                tail.add(k);
+            }
+        }
+
+        result = new RLnumber(sign, tail.size(), tail);
+        return result;
+    }
 }
